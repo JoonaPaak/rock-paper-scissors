9d51848467af7f4f38c7b14101c50d13c88bfbee
Joona Pääkkönen <joonapaak01@gmail.com>
2021-10-27 18:05:04 +0300

    js modification

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-var playerScore = 0;
+//Variable declaration
+let playerScore = 0;
 let computerScore = 0;
 let roundWinner = "";
 
+//Random selector for computers choice
 function computerPlay() {
   decision = ["rock", "paper", "scissors"];
   return decision[Math.floor(Math.random() * decision.length)];
 }
 
+//Loops through playRound function until one of the scores equals to 5 and alerts winner function
 function game() {
   while (playerScore < 5 && computerScore < 5) {
     let computerSelection = computerPlay();
@@ -16,23 +19,33 @@ function game() {
   alert(winner());
 }
 
-function playRound (playerSelection, computerSelection) {
+//One round of the game. Checks who wins and changes scores accordingly
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     roundWinner = "It's a tie!";
-    return roundWinner + " Your score: " + playerScore + " Computer score: " + computerScore;
-  } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
-    roundWinner = "You win!"
+    return (roundWinner + " Your score: " + playerScore + " Computer score: " + computerScore);
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    roundWinner = "You win!";
     playerScore++;
-    return roundWinner + " Your score: " + playerScore + " Computer score: " + computerScore;
-  } else if (playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "rock" && computerSelection === "paper") {
-    roundWinner = "You lose!"
+    return (roundWinner + " Your score: " + playerScore + " Computer score: " + computerScore);
+  } else if (
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "rock" && computerSelection === "paper")
+  ) {
+    roundWinner = "You lose!";
     computerScore++;
-    return roundWinner + " Your score: " + playerScore + " Computer score: " + computerScore;
+    return (roundWinner + " Your score: " + playerScore + " Computer score: " + computerScore);
   } else {
     return "You misspelled your answer!";
   }
 }
 
+//Checks who wins the game after 5 rounds
 function winner() {
   if (playerScore === 5) {
     return "Player Wins!";
