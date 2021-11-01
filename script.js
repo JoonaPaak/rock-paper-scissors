930d8362d1b8e70f3f7639044514e253390346d8
Joona Pääkkönen <joonapaak01@gmail.com>
2021-11-01 16:20:39 +0200

    all ready

--- script.js
+++ script.js
@@ -1,58 +1,64 @@
-//Variable declaration
 let playerScore = 0;
 let computerScore = 0;
-let roundWinner = "";
+let roundWinner = document.getElementById("round-winner");
 
-//Random selector for computers choice
 function computerPlay() {
   decision = ["rock", "paper", "scissors"];
   return decision[Math.floor(Math.random() * decision.length)];
 }
 
-//Loops through playRound function until one of the scores equals to 5 and alerts winner function
-function game() {
-  while (playerScore < 5 && computerScore < 5) {
-    let computerSelection = computerPlay();
-    let playerSelection = window.prompt("Write out rock, paper or scissors to play!").toLowerCase();
-    alert(playRound(playerSelection, computerSelection));
+function winner() {
+  if (playerScore === 5) {
+    roundWinner.innerHTML = "Player Wins!";
   }
-  alert(winner());
+  if (computerScore === 5) {
+    roundWinner.innerHTML = "Computer wins!";
+  }
 }
 
-//One round of the game. Checks who wins and changes scores accordingly
+function updateScore() {
+  const playerScorePara = document.getElementById("player-score");
+  const computerScorePara = document.getElementById("computer-score");
+
+  playerScorePara.textContent = `Player: ${playerScore}`;
+  computerScorePara.textContent = `Computer: ${computerScore}`;
+}
+
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    roundWinner = "It's a tie!";
-    return (roundWinner + " Your score: " + playerScore + " Computer score: " + computerScore);
+  computerSelection = computerPlay();
+  while (playerScore < 5 && computerScore < 5) {
+    if (playerSelection === computerSelection) {
+    roundWinner.innerHTML = "It's a tie!";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    roundWinner = "You win!";
+    roundWinner.innerHTML = "You win!";
     playerScore++;
-    return (roundWinner + " Your score: " + playerScore + " Computer score: " + computerScore);
   } else if (
     (playerSelection === "scissors" && computerSelection === "rock") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "rock" && computerSelection === "paper")
   ) {
-    roundWinner = "You lose!";
+    roundWinner.innerHTML = "You lose!";
     computerScore++;
-    return (roundWinner + " Your score: " + playerScore + " Computer score: " + computerScore);
-  } else {
-    return "You misspelled your answer!";
-  }
+  } break;
+}
+winner();
 }
 
-//Checks who wins the game after 5 rounds
-function winner() {
-  if (playerScore === 5) {
-    return "Player Wins!";
-  }
-  if (computerScore === 5) {
-    return "Computer wins!";
-  }
-}
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (playerSelection) => {
+    playerSelection = button.id;
+    playRound(playerSelection);
+    updateScore();
+  });
+});
 
-console.log(game());
+const restart = document.querySelector("restart");
+  restart.addEventListener("click", () => {
+    console.log(restart);
+  });
+
